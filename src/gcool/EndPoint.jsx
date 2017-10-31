7189502e527021d42115e05cc629bdca880d4f67
Edmundo Rubio <zerphyr.r@gmail.com>
2017-10-30 20:49:04 -0600

    add

--- src/gcool/EndPoint.jsx
+++ src/gcool/EndPoint.jsx
@@ -2,7 +2,7 @@ import ApolloClient, { HttpLink } from 'apollo-client-preset'
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.graph.cool/simple/v1/cj9bi3ihz423e0132laol68zu'
+    uri: 'https://api.graph.cool/simple/v1/cj9buxwdy4lhm0163jl4sadbm'
   })
 })
 
